a06084e15451498d9572e55d7d26c1ac5dcfd4d7
Ann Kettles <ann.a.kettles@gmail.com>
2019-03-24 22:26:35 -0400

    completed goodie bag, minus extra credit

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import ShowCandies from './candies';
-import {Link, Route} from 'react-router-dom';
 import Root from './root';
 import Candies from './candies';
+import {Link, Route} from 'react-router-dom';
 
 export default class NavBar extends React.Component {
 	render() {
 		return (
 			<nav>
-				<div>
-					<Route exact path="/" component={Root} />
-					<Link to="/">Home</Link>
-					<Route path="/candies" component={Candies} />
-					<Link to="/candies">All Candies</Link>
-				</div>
+				<Link to="/">Home</Link>
+				<Link to="/candies">Show Candies</Link>
 			</nav>
 		);
 	}
+}
+
+{
+	/* <Route exact path="/" component={Root} />
+    <Link to="/">Home</Link>
+    <Route path="/candies" component={Candies} />
+    <Link to="/candies">All Candies</Link> */
 }
--- app/components/root.js
+++ app/components/root.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ShowCandies from './candies';
-// import NavBar from './navbar';
-// import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import NavBar from './navbar';
+import {HashRouter as Router, Route} from 'react-router-dom';
 
 const Root = () => {
 	return (
-		<div>
-			<nav> Navigation</nav>
-			<main>
-				<h1>Welcome to the Goodie Bag!</h1>
-				<p>What a nice home page for your goodies!</p>
-				<ShowCandies />
-			</main>
-		</div>
+		<Router>
+			<div>
+				<NavBar />
+				<Route exact path="/candies" component={ShowCandies} />
+				<main>
+					<h1>Welcome to the Goodie Bag!</h1>
+					<p>What a nice home page for your goodies!</p>
+					<ShowCandies />
+				</main>
+			</div>
+		</Router>
 	);
 };
 
